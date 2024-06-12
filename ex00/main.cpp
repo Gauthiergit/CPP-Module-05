aff3247b2ac75da598f690fa5c57ba1ffe0e985d
Gauthier <g.p220498@gmail.com>
2024-06-12 22:07:31 +0200

    ex00

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: gpeyre <gpeyre@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/12 18:05:09 by gpeyre            #+#    #+#             */
-/*   Updated: 2024/06/12 18:24:04 by gpeyre           ###   ########.fr       */
+/*   Updated: 2024/06/12 22:06:19 by gpeyre           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,32 @@ int main()
 {
 	try
 	{
-		Bureaucrat bob("bob", 1);
+		Bureaucrat bob("bob", 0);
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << RED << e.what() << RESET << std::endl;
+	}
+
+	try
+	{
 		Bureaucrat john("john", 150);
 		john.decrementGrade();
 		std::cout << john << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << RED << e.what() << RESET << std::endl;
+	}
+
+	try
+	{
+		Bureaucrat max("max", 5);
+		for(int i = 0; i < 5; i++)
+		{
+			max.incrementGrade();
+			std::cout << max << std::endl;
+		}
 	}
 	catch(const std::exception& e)
 	{
